7d9ced6f373bc49ada35c1f7f7e6c838a48bd766
Arbind Kumar Mahato <you@example.com>
2024-01-14 22:22:54 +0545

    updated-today

--- src/peer/Peer.js
+++ src/peer/Peer.js
@@ -1,47 +1,47 @@
 class Peer {
-    constructor (){
-        if(!this.peer){
-            this.peer = new RTCPeerConnection({
-                iceServers: [
-                    {
-                        urls: [
-                            "stun:stun.services.mozilla.com",
-                            "stun:stun.stunprotocol.org:3478"
-                        ],
-                        username: "@global_arbind.tcp.twillo.com",
-                        credential: "$twillo.com?port=4747"
-                    }
-                ]
-            })
-        }
+  constructor() {
+    if (!this.peer) {
+      this.makePeer();
     }
-    
-    async getOffer(){
-        if(this.peer){
-            const offer = await this.peer.createOffer(); 
-            await this.peer.setLocalDescription(new RTCSessionDescription(offer));
-            return offer;
-        }
+  }
+  async makePeer() {
+    this.peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:global.stun.twilio.com:3478",
+          ],
+        },
+      ],
+    });
+  }
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+      return offer;
     }
-    disconnect(){
-        if(this.peer){
-            this.peer.close();
-            this.peer = null;
-        }
+  }
+  disconnect() {
+    if (this.peer) {
+      this.peer.close();
+      this.makePeer();
     }
-    async connectRemoteOffer(offer){
-        if(this.peer ){
-            await this.peer.setRemoteDescription(offer);
-            const answer = await this.peer.createAnswer();
-            await this.peer.setLocalDescription(new RTCSessionDescription(answer));
-            return answer;
-        }
+  }
+  async connectRemoteOffer(offer) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(offer);
+      const answer = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(answer));
+      return answer;
     }
-    async setRemoteDescription(ans){
-        if(this.peer){
-            await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
-        }
+  }
+  async setRemoteDescription(ans) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
     }
+  }
 }
 
 export default Peer;
