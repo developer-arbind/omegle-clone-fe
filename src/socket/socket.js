cfdf1410d6218fde0d820fd6659bb5262a31a6bc
Arbind Kumar Mahato  <135502954+arbind-mahato@users.noreply.github.com>
2024-01-16 21:17:08 +0545

    Update socket.js

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 
-const socketInstance = io('https://omegle-clone-backend.vercel.app');
+const socketInstance = io('https://omegle-clone-backend.onrender.com');
 socketInstance.connect();
 export {
     socketInstance
